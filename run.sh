022ad25ee150567f7f038fc2b2ae17f7bee5acfa
Kevin Sandermann <Kevin.sandermann@gmail.com>
2019-07-25 17:34:12 +0200

    2019 07 25 01 (#3)
    * added generic aliases to bashrc
    
    * * switched baseimage to ksandermann/multistage-builder
    * kubectl now installed via builder image, not via apt anymore
    * added fping
    
    * refactored build and run scripts
    
    * release 2019-07-25_01

--- run.sh
+++ run.sh
@@ -3,15 +3,16 @@ set -euo pipefail
 IFS=$'\n\t'
 
 
-IMAGE_TAG="ksandermann/cloud-toolbox:latest"
+IMAGE_TAG="latest"
 
 docker run -ti --rm \
     -v ~/.kube:/root/.kube \
+    -v ~/.helm:/root/.helm \
     -v ~/.ssh:/root/.ssh \
     -v ${PWD}:/root/project \
     -v ~/.gitconfig:/root/.gitconfig \
     -v ~/.aws:/root/.aws \
     -v ~/.azure:/root/.azure \
     -v /var/run/docker.sock:/var/run/docker.sock \
-    $IMAGE_TAG \
+    ksandermann/cloud-toolbox:$IMAGE_TAG \
     bash
