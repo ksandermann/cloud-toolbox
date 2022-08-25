0eefcc660ce564e871082143a16758bcb13022d7
Kevin Sandermann <kevin.sandermann@gmail.com>
2022-08-25 22:53:27 +0200

    multi-arch

--- build.sh
+++ build.sh
@@ -2,21 +2,31 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-IMAGE_TAG="2022-07-30_01"
+IMAGE_TAG="2022-08-25_multiarch"
 UPSTREAM_TAG="latest"
 UPSTREAM_TAG2="project"
 
-docker build \
+#https://blog.jaimyn.dev/how-to-build-multi-architecture-docker-images-on-an-m1-mac/
+
+docker buildx build \
     --pull \
-    --no-cache \
+    --platform linux/amd64,linux/arm64 \
+    --push \
     -t ksandermann/cloud-toolbox:$IMAGE_TAG \
     .
 
-docker login
-docker push ksandermann/cloud-toolbox:$IMAGE_TAG
 
-docker tag ksandermann/cloud-toolbox:$IMAGE_TAG ksandermann/cloud-toolbox:$UPSTREAM_TAG
-docker push ksandermann/cloud-toolbox:$UPSTREAM_TAG
+    #--no-cache \
 
-docker tag ksandermann/cloud-toolbox:$IMAGE_TAG ksandermann/cloud-toolbox:$UPSTREAM_TAG2
-docker push ksandermann/cloud-toolbox:$UPSTREAM_TAG2
+
+#docker login
+#docker push ksandermann/cloud-toolbox:$IMAGE_TAG
+#
+#docker tag ksandermann/cloud-toolbox:$IMAGE_TAG ksandermann/cloud-toolbox:$UPSTREAM_TAG
+#docker push ksandermann/cloud-toolbox:$UPSTREAM_TAG
+#
+#docker tag ksandermann/cloud-toolbox:$IMAGE_TAG ksandermann/cloud-toolbox:$UPSTREAM_TAG2
+#docker push ksandermann/cloud-toolbox:$UPSTREAM_TAG2
+#
+#
+#
